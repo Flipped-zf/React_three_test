cd4e8d564c57b6aba5826a4eb7fb31c176e595b7
Flipped <468369392@qq.com>
2022-11-24 17:27:30 +0800

    okok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import state from "./store.js"
 import { useRef , useEffect , Suspense} from 'react'
 import { Html } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import Content from './components/content'
 import './components/CustomMaterial'
+import Plane from './components/Plane'
+import lerp from "lerp"
+import Diamonds from "./diamonds/Diamonds"
+
+function Startup() {
+  const ref = useRef()
+  useFrame(() => (ref.current.material.opacity = lerp(ref.current.material.opacity, 0, 0.025)))
+  return <Plane ref={ref} color="#0e0e0f" position={[0, 0, 200]} scale={[100, 100, 1]} />
+}
 
 function App() {
   const scrollArea = useRef()
   const onScroll = (e) => {
     state.top.current = e.target.scrollTop
   }
-  useEffect(() => {
-    onScroll({ target: scrollArea.current })
-  }, [])
-
+  useEffect(() => void onScroll({ target: scrollArea.current }), [])
   return (
     <>
     <Canvas linear dpr={[1, 2]} orthographic camera={{ zoom: state.zoom, position: [0, 0, 500] }}>
       <Suspense fallback={<Html center className="loading" children="Loading..." />}>
         <Content />
+        <Diamonds />
       </Suspense>
     </Canvas>
       <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
